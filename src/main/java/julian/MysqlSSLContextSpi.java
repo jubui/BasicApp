be37032b3016990add40d8b25949c6f00871d11f
Julian Bui <julian.bui@appian.com>
2024-07-25 23:04:54 -0400

    Custom Trust Store that combines the default trusted certs of SunJSSE and those of my files on disk

--- src/main/java/julian/MysqlSSLContextSpi.java
+++ src/main/java/julian/MysqlSSLContextSpi.java
@@ -11,6 +11,7 @@ import java.security.cert.X509Certificate;
 
 public class MysqlSSLContextSpi extends SSLContextSpi {
   private SSLContext delegate;
+  private TrustManagerFactory tmf;
 
   private void addCert(KeyStore ks, String certPath, String alias) throws CertificateException, FileNotFoundException, KeyStoreException {
     CertificateFactory cf = CertificateFactory.getInstance("X.509");
@@ -22,16 +23,17 @@ public class MysqlSSLContextSpi extends SSLContextSpi {
   }
   public MysqlSSLContextSpi() throws NoSuchAlgorithmException {
     // Initialize with a standard SSLContext implementation
-    System.out.println("JULIAN");
+    System.out.println("JULIAN2");
 //    this.delegate = SSLContext.getInstance("TLS");
 //    SSLParameters sslParameters = delegate.getDefaultSSLParameters();
 //    sslParameters.setProtocols(new String[]{"TLS"});
 
     try {
 
-      TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+       tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
       KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
       ks.load(null); // You don't need the KeyStore instance to come from a file.
+
 
       for (int i = 1; i <= 5; i++) {
         CertificateFactory cf = CertificateFactory.getInstance("X.509");
@@ -43,17 +45,19 @@ public class MysqlSSLContextSpi extends SSLContextSpi {
         ks.setCertificateEntry("mycert" + i, mycert);
       }
 
+      /*
       addCert(ks, "/Users/julian.bui/Downloads/global-bundle.pem", "global-bundle");
+       */
 
       tmf.init(ks);
       for (TrustManager tm : tmf.getTrustManagers()) {
         System.out.println(tm);
       }
       // Create SSLContext with desired SSL/TLS configuration
-      SSLContext sslContext = SSLContext.getInstance("TLS");
+//      SSLContext sslContext = SSLContext.getInstance("TLS");
       // Initialize SSLContext. Example: using default TrustManager and KeyManager
-      sslContext.init(null, tmf.getTrustManagers(), null);
-      delegate = sslContext;
+//      sslContext.init(null, tmf.getTrustManagers(), null);
+//      delegate = sslContext;
     } catch (Exception e) {
       System.out.println("JULIAN, error: " + e);
       e.printStackTrace();
@@ -64,8 +68,26 @@ public class MysqlSSLContextSpi extends SSLContextSpi {
   protected void engineInit(KeyManager[] km, TrustManager[] tm, SecureRandom sr)
       throws KeyManagementException {
     System.out.println("JULIAN");
-    System.out.println("ALREADY INITIALIZED");
-//    delegate.init(km, tm, sr);
+    try {
+
+      SSLContext sslContext = SSLContext.getInstance("TLS", "SunJSSE");
+//      sslContext.init(null, null, null);
+
+      TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+      trustManagerFactory.init((KeyStore)null);
+
+      TrustManager[] tms = trustManagerFactory.getTrustManagers();
+
+      TrustManager[] mergedTms = new TrustManager[]{new CustomTrustManager(tms[0], tmf.getTrustManagers()[0])};
+      sslContext.init(null, mergedTms, sr);
+      delegate = sslContext;
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    } catch (NoSuchProviderException e) {
+      throw new RuntimeException(e);
+    } catch (KeyStoreException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
--- src/main/java/julian/OracleTest.java
+++ src/main/java/julian/OracleTest.java
@@ -1,16 +1,6 @@
 package julian;
 
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.cert.CertificateException;
-import java.security.cert.CertificateFactory;
-import java.security.cert.X509Certificate;
+import java.security.Security;
 import java.sql.Driver;
 import java.util.Properties;
 
@@ -18,6 +8,7 @@ public class OracleTest {
   public void execute() {
     System.out.println(System.getProperty("javax.net.ssl.trustStore"));
 //    setup();
+    Security.insertProviderAt(new MysqlCustomProvider(), 1);
     try {
 //      System.setProperty("javax.net.ssl.trustStore", "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/lib/security/jssecacerts");
 //      System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
@@ -34,57 +25,13 @@ public class OracleTest {
 //          "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/lib/security/jssecacerts");
 
       // USING CACERTS ALSO WORKS, as long as we specify it
-      properties.setProperty("javax.net.ssl.trustStore",
-          "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/lib/security/cacerts");
-      properties.setProperty("javax.net.ssl.trustStoreType","JKS");
-      properties.setProperty("javax.net.ssl.trustStorePassword","changeit");
+//      properties.setProperty("javax.net.ssl.trustStore",
+//          "/Library/Java/JavaVirtualMachines/temurin-17.jdk/Contents/Home/lib/security/cacerts");
+//      properties.setProperty("javax.net.ssl.trustStoreType","JKS");
+//      properties.setProperty("javax.net.ssl.trustStorePassword","changeit");
       d.connect(connectionUrl, properties);
       System.out.println("Connection successful");
     } catch(Exception e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void addCert(KeyStore ks, String certPath, String alias) throws CertificateException, FileNotFoundException, KeyStoreException {
-    CertificateFactory cf = CertificateFactory.getInstance("X.509");
-    System.out.println("Loading: " + certPath);
-    InputStream is = new FileInputStream(certPath);
-    X509Certificate mycert = (X509Certificate) cf.generateCertificate(is);
-
-    ks.setCertificateEntry(alias, mycert);
-  }
-
-  public void setup() {
-    try {
-
-      TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-      KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
-      ks.load(null); // You don't need the KeyStore instance to come from a file.
-
-      for (int i = 1; i <= 5; i++) {
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        String path = "/Users/julian.bui/Downloads/us-east-1-bundle-" + i + ".der";
-        System.out.println("Loading: " + path);
-        InputStream is = new FileInputStream(path);
-        X509Certificate mycert = (X509Certificate) cf.generateCertificate(is);
-
-        ks.setCertificateEntry("mycert" + i, mycert);
-      }
-
-      addCert(ks, "/Users/julian.bui/Downloads/global-bundle.pem", "global-bundle");
-
-      tmf.init(ks);
-      for (TrustManager tm : tmf.getTrustManagers()) {
-        System.out.println(tm);
-      }
-      // Create SSLContext with desired SSL/TLS configuration
-      SSLContext sslContext = SSLContext.getInstance("TLS");
-      // Initialize SSLContext. Example: using default TrustManager and KeyManager
-      sslContext.init(null, tmf.getTrustManagers(), null);
-
-      SSLContext.setDefault(sslContext);
-    } catch (Exception e) {
-      System.out.println("JULIAN, error: " + e);
       e.printStackTrace();
     }
   }
